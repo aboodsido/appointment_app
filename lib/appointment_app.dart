5a07bf3af702ec210215b4235007bc05b87a5197
Abdullah Abo-Sido <odod41907@gmail.com>
2025-03-24 14:02:07 +0200

    launching the app

---
+++ lib/appointment_app.dart
@@ -0,0 +1,28 @@
+import 'package:appointments_app/core/routing/app_router.dart';
+import 'package:appointments_app/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routing/routes.dart';
+
+class AppointmentApp extends StatelessWidget {
+  AppRouter router = AppRouter();
+  AppointmentApp({super.key, required this.router});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: ColorManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        onGenerateRoute: router.generateRoute,
+        initialRoute: Routes.onBoardingScreen,
+      ),
+    );
+  }
+}
